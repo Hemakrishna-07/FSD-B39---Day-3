d70cbd339ed5916140937fd64a90df937b91226a
Hemakrishna-07 <110914190+Hemakrishna-07@users.noreply.github.com>
2022-08-14 17:02:22 +0530

    Add files via upload

---
+++ Resume_JSON.js
@@ -0,0 +1,45 @@
+let myResume={
+    "basics": {
+      "name": "Hemakrishna",
+      "email": "hemky143@gamil.com",
+      "phone": 7299408367,
+      "degree": "B.E",
+      "location": {
+        "address": "No.6 4th street Unity nagar Kavaraipettai",
+        "postalCode": 601206,
+        "city": "Tiruvallur",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+    },
+    "work": [
+      {
+        "company": "EQIC Dies Moulds & Engineers",
+        "position": "Tool Design Engineer",
+        "startDate": "20-05-2021",
+        "endDate": "15-07-2022",
+        "summary": "I work on designing HPDC tools",
+      },
+    ],
+    "education": [
+      {
+        "institution": "Meenakshi College of Engineering",
+        "department": "Mechanical",
+        "studyType": "fulltime",
+        "batch start year": 2015,
+        "batch end year": 2019,
+        "gpa": 6.7,
+      }
+    ],
+    "languages": [
+      {
+        "language": " English, Tamil, Telugu ",
+      }
+    ],
+    "interests": [
+      {
+        "name": "Gaming, Badminton, Photography",
+      }
+    ]
+  }
+  console.log(myResume);
